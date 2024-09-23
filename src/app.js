f4a9da776662eeb1920217d1a6f0b32a21af51af
migueluruguay <miguelangelsilveirafagundez@gmail.com>
2024-09-23 23:36:16 +0000

    cartas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("palos");
+  console.log("values");
+  const palos = [`♦`, `♥`, `♠`, `♣`];
+  const values = [
+    " 2 ",
+    " 3 ",
+    " 4 ",
+    " 5 ",
+    " 6 ",
+    " 7 ",
+    " 8 ",
+    " 9 ",
+    " 10 ",
+    " j ",
+    " q ",
+    " k ",
+    " a ",
+  ];
+  let randomPalos = palos[Math.floor(Math.random() * palos.length)];
+  let randomValue = values[Math.floor(Math.random() * values.length)];
 };
